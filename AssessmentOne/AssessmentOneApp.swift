9252cc02b8d70933ae92a97d872887dd15cf17a3
Pushpendra Kumar <pushpendra@thecolourmoon.com>
2025-05-26 23:46:51 +0530

    Initital Commit

---
+++ AssessmentOne/AssessmentOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssessmentOneApp.swift
+//  AssessmentOne
+//
+//  Created by Pushpendra on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AssessmentOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssessmentOneTests/AssessmentOneTests.swift
@@ -0,0 +1,16 @@
+//
+//  AssessmentOneTests.swift
+//  AssessmentOneTests
+//
+//  Created by Pushpendra on 26/05/25.
+//
+
+import Testing
+
+struct AssessmentOneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
